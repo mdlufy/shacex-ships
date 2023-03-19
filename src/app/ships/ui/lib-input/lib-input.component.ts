abae5a7b98ac9d5569d442128c0c484e3b841356
mdlufy <german.panov.2018@mail.ru>
2023-03-19 21:37:38 +0300

    feat(ui): add lib-ui input

---
+++ src/app/ships/ui/lib-input/lib-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "lib-input",
+    templateUrl: "./lib-input.component.html",
+    styleUrls: ["./lib-input.component.scss"],
+})
+export class LibInputComponent {
+    constructor() {}
+}
