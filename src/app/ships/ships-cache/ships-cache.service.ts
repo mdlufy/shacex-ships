249fe7b0ee2244494af1fdb87b955a52daae10e5
mdlufy <german.panov.2018@mail.ru>
2023-03-19 20:53:45 +0300

    refactor(ships): remove requesting by options and set default api handle

--- src/app/ships/ships-cache/ships-cache.service.ts
+++ src/app/ships/ships-cache/ships-cache.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Observable, shareReplay } from "rxjs";
-import { ShipDto, ShipsResponseDto } from "../ships-data/ship.dto";
+import { ShipDto } from "../ships-data/ship.dto";
 import { ShipsDataService, ShipsListOptions } from "../ships-data/ships-data.service";
 import { ShipOptions } from './../ships-data/ships-data.service';
 
@@ -9,22 +9,22 @@ const UNSUBSCRIBE_ZERO_CONSUMERS = true;
 
 @Injectable()
 export class ShipsCacheService {
-    private shipsCache$ = new Map<string, Observable<ShipsResponseDto | ShipDto>>();
+    private shipsCache$ = new Map<string, Observable<ShipDto | ShipDto[]>>();
 
     constructor(private shipsDataService: ShipsDataService) {}
 
-    public getShips$(options: ShipsListOptions): Observable<ShipsResponseDto> {
+    public getShips$(options: ShipsListOptions): Observable<ShipDto[]> {
         const shipsRequestOptions = JSON.stringify(options);
 
         if (!this.shipsCache$.has(shipsRequestOptions)) {
-            const response = this.shipsDataService.fetchShips$(options).pipe(
+            const response = this.shipsDataService.fetchShips$().pipe(
                 shareReplay({ bufferSize: CACHE_SIZE, refCount: UNSUBSCRIBE_ZERO_CONSUMERS }),
             );
 
             this.shipsCache$.set(shipsRequestOptions, response);
         }
 
-        return this.shipsCache$.get(shipsRequestOptions) as Observable<ShipsResponseDto>;
+        return this.shipsCache$.get(shipsRequestOptions) as Observable<ShipDto[]>;
     }
 
     public getShipByShipId$(options: ShipOptions): Observable<ShipDto> {
--- src/app/ships/ships-data/ships-data.service.ts
+++ src/app/ships/ships-data/ships-data.service.ts
@@ -2,15 +2,14 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { initialApiUrl } from "src/app/config_api";
-import { ShipView } from "../+state/ships-view/ships-view.reducer";
 import { ShipDto, ShipsResponseDto } from "./ship.dto";
 
 export interface ShipsListOptions {
-    page: number;
-    limit: number;
+    page: number | null;
+    limit: number | null;
 }
 
-export interface ShipOptions extends Partial<ShipView> {
+export interface ShipOptions extends Partial<ShipDto> {
     id: string;
 }
 
@@ -18,11 +17,15 @@ export interface ShipOptions extends Partial<ShipView> {
 export class ShipsDataService {
     constructor(private readonly http$: HttpClient) {}
 
-    public fetchShips$(options: ShipsListOptions): Observable<ShipsResponseDto> {
-        const query = {};
-
-        return this.http$.post<ShipsResponseDto>(`${initialApiUrl}/v4/ships/query`, { query, options });
+    public fetchShips$(): Observable<ShipDto[]> {
+        return this.http$.get<ShipDto[]>(`${initialApiUrl}/v4/ships/`);
     }
+
+    // public fetchShips$(options: ShipsListOptions | {} = {}): Observable<ShipsResponseDto> {
+    //     const query = {};
+
+    //     return this.http$.post<ShipsResponseDto>(`${initialApiUrl}/v4/ships/query`, { query, options });
+    // }
 
     public fetchShipByShipId$({ id }: ShipOptions): Observable<ShipDto> {
         return this.http$.get<ShipDto>(`${initialApiUrl}/v4/ships/${id}`);
