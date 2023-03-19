5123a9f0be7480a18022b27788246989e969c7fb
mdlufy <german.panov.2018@mail.ru>
2023-03-19 14:58:43 +0300

    refactor(ships/ship-detail): add tapResponse callback for load ship

--- src/app/ships/ships-detail/+state/ship-detail.store.ts
+++ src/app/ships/ships-detail/+state/ship-detail.store.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { ComponentStore } from "@ngrx/component-store";
+import { ComponentStore, tapResponse } from "@ngrx/component-store";
 import { catchError, map, Observable, of, switchMap, tap } from "rxjs";
 import { LoadingState } from "../../+state/loading-state";
 import { ShipView } from "../../+state/ships-view/ships-view.reducer";
@@ -7,6 +7,7 @@ import { mapShipDtoToShipView } from "../../helpers/ship-mapping.helper";
 import { ShipDto } from "../../ships-data/ship.dto";
 import { ShipOptions } from "../../ships-data/ships-data.service";
 import { ShipsCacheService } from "../../ships-cache/ships-cache.service";
+import { HttpErrorResponse } from "@angular/common/http";
 
 export interface ShipState {
     loadingState: LoadingState;
@@ -36,20 +37,32 @@ export class ShipDetailStore extends ComponentStore<ShipState> {
         super({ loadingState: LoadingState.LOADING, ship: shipInittialState });
     }
 
+    private setShip = this.updater((state, ship: ShipView) => ({
+        loadingState: LoadingState.SUCCESS,
+        ship,
+    }));
+
     public loadShip$ = this.effect((shipOptions$: Observable<ShipOptions>) => {
         return shipOptions$.pipe(
             tap(() => this.patchState({ loadingState: LoadingState.LOADING })),
             switchMap((shipOptions: ShipOptions) =>
                 this.shipsCacheService.getShipByShipId$(shipOptions).pipe(
                     map((ship: ShipDto) => mapShipDtoToShipView(ship)),
+                    tapResponse(
+                        (ship: ShipView) => this.setShip(ship),
+                        () => this.handleErrorResponse()
+                    ),
                     tap((ship: ShipView) => this.setState({ loadingState: LoadingState.SUCCESS, ship })),
-                    catchError(() => {
-                        this.patchState({ loadingState: LoadingState.LOADING_ERROR });
-
-                        return of(null);
-                    })
+                    catchError((error: HttpErrorResponse) => this.handleErrorResponse())
                 )
             )
         );
     });
+
+
+    private handleErrorResponse(): Observable<ShipView | null> {
+        this.patchState({ loadingState: LoadingState.LOADING_ERROR });
+
+        return of(null);      
+    }
 }
