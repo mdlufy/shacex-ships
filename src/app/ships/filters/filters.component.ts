059ed87145459589081c371ac89560b393a01467
mdlufy <german.panov.2018@mail.ru>
2023-03-25 20:53:43 +0300

    feat(ships/filters): use checkbox-list component from mdlufy-ui library

--- src/app/ships/filters/filters.component.ts
+++ src/app/ships/filters/filters.component.ts
@@ -15,9 +15,6 @@ import { distinctUntilChanged, Observable, takeUntil, tap } from "rxjs";
 import { ShipsFilters } from "../+state/ships-filters/ships-filters.reducer";
 import { FilterShipType } from "./filter-types";
 
-// TODO: оформить фильтр чекбоксов как библиотечное решение в mdlufy-ui: 
-// вынести в отдельный компонент, реализующий ControlValueAccessor
-
 @Component({
     selector: "app-filters",
     templateUrl: "./filters.component.html",
@@ -33,22 +30,6 @@ export class FiltersComponent implements OnInit, OnChanges {
 
     @Output() updateFilters = new EventEmitter<ShipsFilters>();
 
-    public get currentShipPorts(): string[] {
-        return this._currentShipPorts;
-    }
-
-    public set currentShipPorts(ports: string[]) {
-        this._currentShipPorts = ports;
-    }
-
-    public get shipsPortsValue(): string {
-        return this.currentShipPorts.length ? `Выбраны ${this.currentShipPorts.length}` : '';
-    }
-
-    public get shipsPortsClasses(): string {
-        return this.currentShipPorts.length ? 'input-blue-border' : '';
-    }
-
     public readonly SHIP_NAME = FilterShipType.SHIP_NAME;
     public readonly SHIP_PORTS_ITEMS = FilterShipType.SHIP_PORTS_ITEMS;
     public readonly SHIP_TYPE = FilterShipType.SHIP_TYPE;
@@ -58,8 +39,6 @@ export class FiltersComponent implements OnInit, OnChanges {
         [FilterShipType.SHIP_PORTS_ITEMS]: new FormControl([], { nonNullable: true }),
         [FilterShipType.SHIP_TYPE]: new FormControl(''), 
     });
-
-    private _currentShipPorts: string[];
 
     constructor(@Inject(DestroyService) private readonly destroy$: Observable<void>) {}
 
@@ -73,32 +52,12 @@ export class FiltersComponent implements OnInit, OnChanges {
         this.initFiltersSubscription();
     }
 
-    public isCheckboxChecked(value: string): boolean {
-        return this.currentShipPorts.includes(value);
-    }
-
-    public onCheckboxChange(shipPort: string): void {
-        const shipPortsItems: string[] = this.filtersForm.get(this.SHIP_PORTS_ITEMS)?.value;
-
-        if (!shipPortsItems.includes(shipPort)) {
-            this.filtersForm.patchValue({ [FilterShipType.SHIP_PORTS_ITEMS]: [...shipPortsItems, shipPort] });
-
-            return;
-        }
-
-        this.filtersForm.patchValue({ [FilterShipType.SHIP_PORTS_ITEMS]: shipPortsItems.filter(port => port !== shipPort) });
-    }
-
     public resetFilters(): void {
-        this.currentShipPorts = [];
-
         this.filtersForm.reset();
     }
 
     private setFiltersFormFields(filtersForm: ShipsFilters): void {
         this.filtersForm.setValue(filtersForm, { emitEvent: false });
-
-        this.currentShipPorts = filtersForm.shipPortsItems;
     }
 
     private initFiltersSubscription() {
@@ -109,5 +68,4 @@ export class FiltersComponent implements OnInit, OnChanges {
         )
         .subscribe();
     }
-
 }
--- src/app/ships/ships.module.ts
+++ src/app/ships/ships.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
-import { InputModule, PaginationModule, RadioListModule } from "mdlufy-ui";
+import { InputModule, PaginationModule, RadioListModule, CheckboxListModule } from "mdlufy-ui";
 import { shipsFiltersReducer } from "./+state/ships-filters/ships-filters.reducer";
 import { FEATURE_SHIPS_FILTERS } from "./+state/ships-filters/ships-filters.selectors";
 import { ShipsViewEffects } from "./+state/ships-view/ships-view.effects";
@@ -35,6 +35,7 @@ const EFFECTS_LIST = [ShipsViewEffects];
         PaginationModule,
         InputModule,
         RadioListModule,
+        CheckboxListModule,
         StoreModule.forFeature(FEATURE_SHIPS_VIEW, shipsViewReducer),
         StoreModule.forFeature(FEATURE_SHIPS_FILTERS, shipsFiltersReducer),
         EffectsModule.forFeature(EFFECTS_LIST),
